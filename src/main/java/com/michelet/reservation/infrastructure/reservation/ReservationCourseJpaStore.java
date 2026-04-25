7b1532bfe5119eed5a9a733f448146fcdad4909d
githyj-jang <hoo9268@gmail.com>
2026-04-26 00:45:11 +0900

    fix:추가적인 코드 설정이 없는 상태에서 실행 불가 문제 해결

--- src/main/java/com/michelet/reservation/infrastructure/reservation/ReservationCourseJpaStore.java
+++ src/main/java/com/michelet/reservation/infrastructure/reservation/ReservationCourseJpaStore.java
@@ -1,7 +1,5 @@
 package com.michelet.reservation.infrastructure.reservation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationCourseJpaStore extends JpaRepository {
+public interface ReservationCourseJpaStore {
 
 }
--- src/main/java/com/michelet/reservation/infrastructure/reservation/ReservationJpaStore.java
+++ src/main/java/com/michelet/reservation/infrastructure/reservation/ReservationJpaStore.java
@@ -1,7 +1,5 @@
 package com.michelet.reservation.infrastructure.reservation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationJpaStore extends JpaRepository {
+public interface ReservationJpaStore {
 
 }
